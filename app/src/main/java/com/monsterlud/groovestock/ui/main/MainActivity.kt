3b274adbe922510286095364186ffdef8c15e31f
Douglas Monsalud <monsterlud@gmail.comgit config --global user.email monsterlud@gmail.com>
2021-09-29 14:48:53 -0700

    updated manifest and build.gradle dependencies

--- app/src/main/java/com/monsterlud/groovestock/ui/main/MainActivity.kt
+++ app/src/main/java/com/monsterlud/groovestock/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.monsterlud.groovestock.R
@@ -20,11 +21,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView
-            = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+            = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         val navHostFragment
             = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
         val navController
             = navHostFragment?.findNavController()
+        if (navController != null) {
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        }
 
 
     }
